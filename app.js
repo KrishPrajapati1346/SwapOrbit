96bb076bda362f9dc36b0e92ec5c11ecea03a3b5
saumil <saumiliant2021@gmail.com>
2025-06-03 23:05:52 +0530

    Updated app.js with Google OAuth fix

--- app.js
+++ app.js
@@ -118,8 +118,8 @@ const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 passport.use(new GoogleStrategy({
     clientID: CLIENT_ID,
     clientSecret: CLIENT_SECRET,
-    // callbackURL: "https://project-ln3i.onrender.com/auth/google/callback"
-    callbackURL: "/auth/google/callback",
+    callbackURL: "https://project-ln3i.onrender.com/auth/google/callback"
+    // callbackURL: "/auth/google/callback",
 
 },
 async function(accessToken, refreshToken, profile, done) {
